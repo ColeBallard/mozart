a83485b8301177eddc8cd1622bba761a2598fa2a
coleb <cole.ballard97@gmail.com>
2021-04-06 15:45:05 -0500

    successful playlist data retrieval

--- flask/server.py
+++ flask/server.py
@@ -1,16 +1,18 @@
+import logging
 from flask import (
-    Blueprint, flash, redirect, render_template, request, url_for, session
+    Flask, redirect, render_template, request, url_for, session
 )
 import spotipy
 from spotipy import oauth2
 
-bp = Blueprint('mozart', __name__)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = ''
 
 CACHE = '.spotifycache'
 # Reads client id and client secret from environment variables
 sp_oauth = oauth2.SpotifyOAuth(cache_path=CACHE )
 
-@bp.route('/', methods=['GET'])
+@app.route('/', methods=['GET'])
 def login():
     # If auth token is already cached and not expired, use that else redirect
     # user to login or refresh token
@@ -18,24 +20,28 @@ def login():
     if token_info and not sp_oauth.is_token_expired(token_info):
         access_token = token_info['access_token']
         session['access_token'] = access_token
-        return redirect(url_for('mozart.playlist'))
+        return render_template('playlist.html')
     else:
         login_url = sp_oauth.get_authorize_url()
         return redirect(login_url)
 
 # After generating code, Spotify redirects to this endpoint
-@bp.route('/oauth/callback', methods=['GET'])
+@app.route('/oauth/callback', methods=['GET'])
 def set_token():
     code = request.args['code']
     token_info = sp_oauth.get_access_token(code)
     access_token = token_info['access_token']
     session['access_token'] = access_token
-    return redirect(url_for('mozart.playlist'))
+    return render_template('playlist.html')
 
 # Get user inputted playlist
-@bp.route('/playlist', methods=['GET'])
+@app.route('/playlist', methods=['POST'])
 def playlist():
     access_token = session['access_token']
     sp_api = spotipy.Spotify(access_token)
-
+    uri = request.form['spotify-uri']
+    playlist = sp_api.playlist(uri)
+    print(playlist)
     return render_template('playlist.html')
+    
+    
