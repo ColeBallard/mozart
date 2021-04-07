264c95f87ce87a50bb19f247d7cac5710e0546c1
coleb <cole.ballard97@gmail.com>
2021-04-06 20:54:39 -0500

    prepare for heroku deployment

--- flask/app.py
+++ flask/app.py
@@ -9,13 +9,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = ''
 
 CACHE = '.spotifycache'
-# Reads client id and client secret from environment variables
 sp_oauth = oauth2.SpotifyOAuth(cache_path=CACHE )
 
 @app.route('/', methods=['GET'])
 def login():
-    # If auth token is already cached and not expired, use that else redirect
-    # user to login or refresh token
     token_info = sp_oauth.get_cached_token()
     if token_info and not sp_oauth.is_token_expired(token_info):
         access_token = token_info['access_token']
@@ -25,7 +22,6 @@ def login():
         login_url = sp_oauth.get_authorize_url()
         return redirect(login_url)
 
-# After generating code, Spotify redirects to this endpoint
 @app.route('/oauth/callback', methods=['GET'])
 def set_token():
     code = request.args['code']
@@ -34,7 +30,6 @@ def set_token():
     session['access_token'] = access_token
     return render_template('playlist.html')
 
-# Get user inputted playlist
 @app.route('/playlist', methods=['POST'])
 def playlist():
     access_token = session['access_token']
