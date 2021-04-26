fcc9beebe5e5065322fa0b4ec23998e28b76075e
coleb <cole.ballard97@gmail.com>
2021-04-26 13:27:44 -0600

    fixed indentation

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,6 @@
 import os
 from flask import (
-    Flask, redirect, render_template, request, url_for, session
+  Flask, redirect, render_template, request, url_for, session
 )
 import spotipy
 from spotipy import oauth2
@@ -16,36 +16,39 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 CACHE = '.spotifycache'
-sp_oauth = oauth2.SpotifyOAuth(cache_path=CACHE )
+sp_oauth = oauth2.SpotifyOAuth(cache_path=CACHE)
+
+# def validate_playlist():
+
 
 @app.route('/', methods=['GET'])
 def login():
-    token_info = sp_oauth.get_cached_token()
-    if token_info and not sp_oauth.is_token_expired(token_info):
-        access_token = token_info['access_token']
-        session['access_token'] = access_token
-        return render_template('playlist.html')
-    else:
-        login_url = sp_oauth.get_authorize_url()
-        return redirect(login_url)
-
-@app.route('/oauth/callback', methods=['GET'])
-def set_token():
-    code = request.args['code']
-    token_info = sp_oauth.get_access_token(code)
+  token_info = sp_oauth.get_cached_token()
+  if token_info and not sp_oauth.is_token_expired(token_info):
     access_token = token_info['access_token']
     session['access_token'] = access_token
     return render_template('playlist.html')
+  else:
+    login_url = sp_oauth.get_authorize_url()
+    return redirect(login_url)
+
+@app.route('/oauth/callback', methods=['GET'])
+def set_token():
+  code = request.args['code']
+  token_info = sp_oauth.get_access_token(code)
+  access_token = token_info['access_token']
+  session['access_token'] = access_token
+  return render_template('playlist.html')
 
 @app.route('/playlist', methods=['POST'])
 def playlist():
-    access_token = session['access_token']
-    sp_api = spotipy.Spotify(access_token)
-    url = request.form['spotify-url']
-    # playlist = sp_api.playlist(uri)
-    # print(playlist['tracks']['total'])
-    # sav.download(uri, query_type=Type.PLAYLIST)
-    return render_template('playlist.html')
+  access_token = session['access_token']
+  sp_api = spotipy.Spotify(access_token)
+  url = request.form['spotify-url']
+  # playlist = sp_api.playlist(uri)
+  # print(playlist['tracks']['total'])
+  # sav.download(uri, query_type=Type.PLAYLIST)
+  return render_template('playlist.html')
     
 if __name__ == '__main__':
-    app.run(threaded=True, port=5000)
+  app.run(threaded=True, port=5000)
